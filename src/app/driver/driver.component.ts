aeb8ef3a4e62db0d4f4d356a12ee8c10165d68af
MibAX Instructor <instructors@mibax.com>
2024-04-18 17:47:28 +0300

    - Added spinner to drivers page - Fixed star rating position in drivers table

--- src/app/driver/driver.component.ts
+++ src/app/driver/driver.component.ts
@@ -4,6 +4,7 @@ import { DriverModel } from '../models/drivers/driver.model';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Gender } from '../enums/gender.enum';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-driver',
@@ -18,6 +19,7 @@ export class DriverComponent implements OnInit {
   constructor(
     private driverSvc: DriverService,
     private snackBar: MatSnackBar,
+    private spinner: NgxSpinnerService,
   ) { }
 
   ngOnInit(): void {
@@ -35,10 +37,13 @@ export class DriverComponent implements OnInit {
 
   private loadDrivers(): void {
 
+    this.spinner.show();
+
     this.driverSvc.getDrivers().subscribe({
       next: (driversFromApi: DriverModel[]) => {
 
         this.drivers = driversFromApi;
+        this.spinner.hide();
       },
       error: (err: HttpErrorResponse) => {
         this.snackBar.open("Couldn't get cars. Please contact system admin.", "Ok");
